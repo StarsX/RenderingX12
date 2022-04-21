40cb94278831e5a2d73bd2b453eb3a88691ecec1
StarsX <X-Universe@outlook.com>
2022-04-21 16:25:30 +0800

    Remove old resources and XUSG binaries

--- Bin/Assets/Bright/Stars.cmd
+++
@@ -1,2 +0,0 @@
-..\ContentExporter -windowsd3d10 -compressednormaltype float16_4 -character -exportanimations+ -computevertextangents+ -exportbinormals- -generatetexturemips+ -verbose "Stars.fbx" > Stars.log
-pause
