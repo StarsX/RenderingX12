6b4ea301dceb4101d4947ba8c3aedca484a0729e
StarsX <X-Universe@outlook.com>
2022-04-21 16:30:43 +0800

    Use Kajiya-Kay to shade hair

---
+++ Bin/Assets/Bright/Stars.cmd
@@ -0,0 +1,2 @@
+..\ContentExporter -windowsd3d10 -compressednormaltype float16_4 -character -exportanimations+ -computevertextangents+ -exportbinormals- -generatetexturemips+ -verbose "Stars.fbx" > Stars.log
+pause
---
+++ Bin/Assets/Island/Island.cmd
@@ -0,0 +1,2 @@
+..\ContentExporter -windowsd3d10 -compressednormaltype float16_4 -computevertextangents+ -exportbinormals- -generatetexturemips+ -verbose "Island.fbx"
+pause
